342ce4286f1d6ef4b28344cb0b70943ee5238948
uyab <bayu.hendra@javan.co.id>
2013-09-26 13:32:59 +0700

    enhance login/logout process

--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -2,30 +2,30 @@
 
 class SiteController extends BaseController {
 
-	public function showLogin()
+	public function getLogin()
 	{
         $this->layout = View::make("site.login");
         $this->layout->extend = 'layouts.empty';
 	}
 
-    public function actionLogin()
+    public function postLogin()
     {
         $user = array(
             'username' => Input::get('username'),
             'password' => Input::get('password')
         );
         if (Auth::attempt($user)) {
-            return Redirect::to('demo/dashboard-git')
+            return Redirect::route('home')
                 ->with('flash_notice', 'You are successfully logged in.');
         }
 
         // authentication failure! lets go back to the login page
-        return Redirect::to('login')
+        return Redirect::route('login')
             ->with('flash_error', 'Your username/password combination was incorrect.')
             ->withInput();
     }
 
-    public function actionLogout()
+    public function getLogout()
     {
         Auth::logout();
         return Redirect::route('login')
--- app/routes.php
+++ app/routes.php
@@ -11,13 +11,22 @@
 |
 */
 
-Route::get('/', 'SiteController@showLogin');
-Route::get('/login', 'SiteController@showLogin');
-Route::post('/login', array('as' => 'login', 'uses' => 'SiteController@actionLogin'));
-Route::get('/logout', array('as' => 'logout', 'uses' => 'SiteController@actionLogout'));
+Route::group(array('before' => 'guest'), function()
+{
+    Route::get('/', array('as' => 'login', 'uses' => 'SiteController@getLogin'));
 
-Route::get('/demo/dashboard', array('as' => 'home', 'uses' => 'DemoController@showPage'));
+    Route::post('/login', 'SiteController@postLogin');
+});
 
-Route::get('/demo/{page}', 'DemoController@showPage');
-Route::post('/form/addUser', 'FormController@addUser');
+Route::group(array('before' => 'auth'), function()
+{
+    Route::get('/logout', array('as' => 'logout', 'uses' => 'SiteController@getLogout'));
+
+    Route::get('/demo/dashboard', array('as' => 'home', 'uses' => 'DemoController@showPage'));
+
+    Route::get('/demo/{page}', 'DemoController@showPage');
+
+    Route::post('/form/addUser', 'FormController@addUser');
+
+});
 
